61bd767e006a2eacdd6440bc417758ca3276e110
Jxkeorton <jakeorton99@gmail.com>
2024-10-26 21:13:40 +0100

    Add axios , Add usecontext hooks, Add sign in signup forms

---
+++ src/api/axiosDefault.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL ="https://drfapi-code-institute-76c4941a10d3.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
