63e409ce905dc91fd9762d2a5e7ebd623a364ff1
Jxkeorton <jakeorton99@gmail.com>
2024-10-26 22:37:15 +0100

    Add axios interceptors

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL ="https://drfapi-code-institute-76c4941a10d3.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
